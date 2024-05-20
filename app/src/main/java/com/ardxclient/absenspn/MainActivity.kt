ec5a08d656250a49913190e7b41bf4721dc959e1
Ibnu Putra <azxarblog@gmail.com>
2024-05-21 00:30:30 +0700

    update bottom navigation icon style and added all tab fragment

--- app/src/main/java/com/ardxclient/absenspn/MainActivity.kt
+++ app/src/main/java/com/ardxclient/absenspn/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import com.ardxclient.absenspn.databinding.ActivityMainBinding
 import com.ardxclient.absenspn.fragment.JadwalFragment
 import com.ardxclient.absenspn.fragment.PresensiFragment
+import com.ardxclient.absenspn.fragment.ProfileFragment
+import com.ardxclient.absenspn.fragment.RekapFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         // Define Fragment
         val presensiFragment = PresensiFragment()
         val jadwalFragment = JadwalFragment()
+        val rekapFragment = RekapFragment()
+        val profileFragment = ProfileFragment()
 
         // Handle Deafult State
         setCurrentFragment(presensiFragment)
@@ -33,6 +37,14 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.tab_jadwal -> {
                     setCurrentFragment(jadwalFragment)
+                    true
+                }
+                R.id.tab_rekap -> {
+                    setCurrentFragment(rekapFragment)
+                    true
+                }
+                R.id.tab_profile -> {
+                    setCurrentFragment(profileFragment)
                     true
                 }
                 else -> false
