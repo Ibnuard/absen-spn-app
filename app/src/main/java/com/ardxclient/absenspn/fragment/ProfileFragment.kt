00d0c8d37e14bc6ff24ac1604b3167f4c1c3ee61
Ibnu Putra <azxarblog@gmail.com>
2024-05-23 00:40:26 +0700

    update profile data

--- app/src/main/java/com/ardxclient/absenspn/fragment/ProfileFragment.kt
+++ app/src/main/java/com/ardxclient/absenspn/fragment/ProfileFragment.kt
@@ -6,9 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ardxclient.absenspn.R
+import com.ardxclient.absenspn.databinding.FragmentProfileBinding
+import com.ardxclient.absenspn.model.response.UserLoginResponse
+import com.ardxclient.absenspn.utils.SessionUtils
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    private lateinit var binding: FragmentProfileBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentProfileBinding.bind(view)
+
+        // Load User Session
+        val userSession = SessionUtils.getUser(requireContext())
+
+        onLoadProfile(userSession)
+    }
+
+    private fun onLoadProfile(session: UserLoginResponse?) {
+        with(binding){
+            tvName.text = session?.nama
+            tvNIM.text = session?.nim.toString()
+            tvStatus.text = session?.status
+            tvKelas.text = session?.kelas
+            tvTahunMasuk.text = session?.tahun_masuk.toString()
+            tvWaliKelas.text = session?.wali_kelas
+            tvTanggalLahir.text = session?.tanggal_lahir
+            tvTempatLahir.text = session?.tempat_lahir
+            tvNomorHP.text = session?.nomor_telp.toString()
+        }
     }
 }
